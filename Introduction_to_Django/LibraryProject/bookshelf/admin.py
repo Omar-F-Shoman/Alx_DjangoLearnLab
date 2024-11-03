3817a0cf1067757f16881811cbffda4db96d7b02
Omar Fathi Shoman <omar.f.shoman@gmail.com>
2024-11-03 21:08:30 +0200

    Integrate Book model with Django admin interface and customize the display.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Register the Book model with the admin site
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Customize the display columns
+    list_filter = ('author',)  # Enable filtering by author
+    search_fields = ('title', 'author')  # Enable search functionality
--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    # Add docstring and improve the response
+    """View function for the home page of the site."""
+    return HttpResponse("<h1>Welcome to my library!</h1>")
