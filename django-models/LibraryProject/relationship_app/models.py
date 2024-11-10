2fd96df0f5fd90bf3a1bfc1a4c6ef89f897f2aa1
Omar Fathi Shoman <omar.f.shoman@gmail.com>
2024-11-10 19:56:28 +0200

    Add models with advanced relationships in relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,9 +6,14 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     # Add other fields as necessary
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Publisher)
     # Add other fields as necessary
 
 class Library(models.Model):
