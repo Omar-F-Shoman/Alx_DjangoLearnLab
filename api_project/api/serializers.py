1a54f072681887d6e1ff918c9babaf41d49f7d31
Omar Fathi Shoman <omar.f.shoman@gmail.com>
2024-11-23 17:22:53 +0200

    create api_project

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
-from .models import Book
+from rest_framework import serializers 
+from .models import Book  # Import the Book model
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
